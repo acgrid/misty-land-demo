e8490f0b38c684ce7df170d42c8b70aa825ddc5f
acgrid <acgrid@gmail.com>
2019-07-03 12:28:51 +0800

    Add tutorial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,11 @@
   <div class="home">
     <router-link class="button" v-if="read" :to="{name: 'quiz'}">曾经阅读过，点击跳过</router-link>
     <img alt="Logo" src="../assets/logo.png">
+    <h4>阵营与职业</h4>
+    <img src="http://misty-land-1253523747.file.myqcloud.com/tutorial/1.png" alt="阵营与职业" />
+    <h4>系统介绍</h4>
+    <img src="http://misty-land-1253523747.file.myqcloud.com/tutorial/2.png" alt="系统介绍" />
+    <img src="http://misty-land-1253523747.file.myqcloud.com/tutorial/3.png" alt="系统介绍" />
     <button v-if="!read" @click="complete">已经阅读完毕，准备从新手村毕业</button>
     <button v-if="read" @click="reset">重置进度</button>
   </div>
